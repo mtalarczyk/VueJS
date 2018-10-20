42262e1577db51a68d2b19f46522755c5ce94e88
Michal <mtalarczyk88@gmail.com>
2018-10-20 15:49:11 +0200

    [#5.1]

---
+++ part2/my-app3/src/components/ProductListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>{{ item.name }}</li>
+</template>
+
+<script>
+  export default {
+    name: "ProductListItem",
+    // The case is, we have to define props (inputs)
+    props: {
+      // Here we also added some basic props validation
+      item: {
+        type: Object
+      }
+    }
+  };
+</script>
+
+<style scoped>
+</style>
